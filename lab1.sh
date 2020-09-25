8be8640ae24848b28154e0520813533606e022a7
EgorGrib <egor111orexov@gmail.com>
2020-09-25 15:09:10 +0300

    fixed

--- lab1.sh
+++ lab1.sh
@@ -24,64 +24,46 @@ fi
 }
 
 function interactive() {
-actions=( "${module_is_here[@]}" "help" "exit" )
+actions=( "calc" "log" "reverse" "search" "strlen" "help" "exit" )
 while :
 do
-	echo "Choose one of the command:"
-	i="0"
-	for m in ${actions[@]}; do
-		echo "$i. $m"
-		i=$(($i+1))
-		done	
+	echo -e "Choose one of the commands:\n0. calc\n1. log\n2. reverse\n3. search\n4. strlen\n5. help\n6. exit"
 	echo -e "\nEnter number or name of command: "
 	read command
-	if [[ $command =~ ^[0-9]+$ ]] 
-		then
-		if [[ $command -lt ${#actions[@]} ]] && [[ $command -ge 0 ]] 
-		then m=${actions[$command]}
-	       	else echo "Error: This command is not in the list"
-		continue
- 		fi
+	if [[ $command =~ ^[0-9]+$ ]]
+		then if [[ $command -lt ${#actions[@]} ]] && [[ $command -ge 0 ]]
+			then c=${actions[$command]}
+		else echo "Error: Unknown command"
+	continue
+ 	fi
 	else 	
 		if [[ ${actions[@]} =~ ("$command") ]] 
-		then m="$command"
+		then c="$command"
 		else echo "Error: Unknown command"
 		continue
 		fi
 	fi
-	if ! [[ "log" =~ ("$m") || "help" =~ ("$m") ]] 
+	if ! [[ "log" =~ ("$c") || "help" =~ ("$c") ]] 
 	then echo "Enter arguments:"
-	read first second third fourth
+	read arg1 arg2 arg3 arg4
 	fi
-	case $m in
-	calc|search|reverse|log)
-		echo "$(module $m $first $second $third $fourth)"
+	case $c in
+	calc|search|reverse|log|strlen)
+		echo "$(module $c $arg1 $arg2 $arg3 $arg4)"
 		continue
 		;;
 	help)
 		echo $(cat /home/user/lab1/help.txt)
 		;;
 	"exit")
-		if ! [[ -z "$first" ]]
-	       	then exit $first
-			else exit 0
+		if ! [[ -z "$arg1" ]]
+	       	then exit $arg1
+		else exit 0
 		fi
 		;;
 	esac
 done
 }
-
-all_modules=("calc" "log" "reverse" "search" "strlen")
-for t in ${all_modules[@]}; do
-	if [[ -e "/home/user/lab1/modules/$t.sh" ]]
-		then module_is_here+=( "$t" )
-		else module_not_found+=( "$t" )
-	fi
-done
-
-if ! [[ -z "$module_not_found" ]]
-	then echo "Some modules were not founded: ${module_not_found[@]}">&2
-fi
 
 if [[ $# -eq 0 ]]
 then 
@@ -93,17 +75,12 @@ case $1 in
 calc|search|reverse|strlen|log)
 	if [[ $# == 1 ]] && [[ $1 != "log" ]]
 		then exit -2
-	fi
-	if [[ ${module_is_here[@]} =~ ($1) ]]
-		then	
-		echo "$(module $1 "$2" $3 $4 $5 $6)"
-		exit $?
-	else echo "You cant run a module that wasnt found">&2
-	exit -2
-	fi
+	fi	
+	echo "$(module $1 "$2" $3 $4 $5 $6)"
+	exit $?
 	;;
 "exit")
-	([[ $# -eq 1 ]] && exit 0 || ([[ $2 =~ ^[-]?[0-9]+$ ]] && exit $2 || echo "Error: you need enter the return code">&2; exit -1))
+	[[ $# -eq 1 ]] && exit 0 || exit $2 2> /dev/null
 	;;
 help)
 	echo $(cat /home/user/lab1/help.txt)
