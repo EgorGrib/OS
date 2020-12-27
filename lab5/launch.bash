5a9231a95ce8cb1f559c77d8bd79f960b38c6c04
root <root@localhost.localdomain>
2020-12-27 23:40:51 +0300

    hmm

--- lab5/launch.bash
+++ lab5/launch.bash
@@ -6,8 +6,8 @@ while true
 do
 	info=$(top -b -n 1 | grep "$pid")
 	info=$(echo -e $info | head -8 | awk '{print ""$11" "$10" "$5" "$6" "$7" "$9""}')
-	free=$(top -b -n 1 -o %MEM | head -4 | tail -n +4 | awk '{print ""$6""}')
-	swap=$(top -b -n 1 -o %MEM | head -5 | tail -n +5 | awk '{print ""$7""}')
-	echo "$info $free $swap" >> data
+	mem=$(top -bn 1 | grep "MiB Mem" | awk '{print $8}')
+	swap=$(top -bn 1 | grep "MiB Swap" | awk '{print $7}')
+	echo "$info $mem $swap" >> data
 	sleep 1
 done
